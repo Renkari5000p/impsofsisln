63778aa928ca1cc91b69f4b76e8ed491dee28155
Renkari5000p <gallardo.carlos.2024@cbtis-246.edu.mx>
2025-03-21 15:15:16 -0600

    Update reto_03.py

--- Parcial2/reto_03/reto_03.py
+++ Parcial2/reto_03/reto_03.py
@@ -1,7 +1,6 @@
-"""
-Usaremos este script para enseñar Python a principiantes absolutos.
+"""    
+Usaremos este script para enseñar Python a principiantes.
 El script es un ejemplo de Fizz-Buzz implementado en Python.
-
 El problema de FizzBuzz:
 Para todos los enteros entre 1 y 99 (incluidos ambos):
     # imprimir fizz para múltiplos de 3
@@ -10,16 +9,18 @@ Para todos los enteros entre 1 y 99 (incluidos ambos):
 """
 
 def fizzbuzz(max_num):
-   
+
+
     for i in range(1,max_num):
-        
+        # % o modulo 
         if i%3==0 and i%5==0:
             print(i,"fizzbuzz")
         elif i%3==0:
             print(i,"fizz")
+            
         elif i%5==0:
             print(i,"Buzz")
 
-#----Iniciar el script
+#iniciar el script
 if __name__=='__main__':
-    fizzbuzz('16')
+    fizzbuzz(100)
